6de0d8908a5c1005c4b0229b073042bdaecb0429
deonslowcole <dcole@fullsail.edu>
2016-04-09 23:03:01 -0400

    Initial Commit

---
+++ ColeDeonslow_CE4Adaptive LayoutProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColeDeonslow_CE4Adaptive LayoutProject
+//
+//  Created by Deon Cole on 4/9/16.
+//  Copyright © 2016 Deon Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
